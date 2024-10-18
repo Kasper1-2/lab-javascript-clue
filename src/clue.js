886eb780ac90dcc966b1c145ccc698c6621ac9d6
Diego Cisneros <dcisnerosg28@gmail.com>
2024-10-18 16:22:17 +0200

    updates

--- src/clue.js
+++ src/clue.js
@@ -2,11 +2,18 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+    {firstName: "", lastName: "", occupation: "", age: 45, description: "", image:"", color:""},
+    {firstName: "", lastName: "", occupation: "", age: 26, description: "", image:"", color:""},
+    {firstName: "", lastName: "", occupation: "", age: 22, description: "", image:"", color:""},
+    {firstName: "", lastName: "", occupation: "", age: 31, description: "", image:"", color:""},
+    {firstName: "", lastName: "", occupation: "", age: 36, description: "", image:"", color:""},
+    {firstName: "", lastName: "", occupation: "", age: 62, description: "", image:"", color:""},    
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [["Dining Room", "Conservatory", "Kitchen", "Study", "Library", "Billiard Room", "Lounge", "Ballroom", "Hall", "Spa", "Living Room", "Observatory", "Theater", "Guest House", "Patio"];];
 
 // Weapons Array
 
